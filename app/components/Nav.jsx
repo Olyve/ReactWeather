e4e39858c1a5833b76d6744a0b155f3142794393
Sam Galizia <samgalizia@gmail.com>
2017-05-02 11:43:06 -0400

    Added Nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,13 @@ let Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault();
 
-    alert('Not yet wired up');
+    let location = this.refs.search.value;
+    let encodedLocation = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = `#/?location=${encodedLocation}`
+    }
   },
   render: function () {
     return (
@@ -21,7 +27,7 @@ let Nav = React.createClass({
         <div className='top-bar-right'>
           <form onSubmit={this.onSearch}>
             <ul className='menu'>
-              <li><input type='search' placeholder='Search weather by city'/></li>
+              <li><input type='search' placeholder='Search weather by city' ref='search'/></li>
               <li><input type='submit' className='button' value='Get Weather'/></li>
             </ul>
           </form>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,7 +16,9 @@ let Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function (temp) {
@@ -31,6 +33,22 @@ let Weather = React.createClass({
         errorMessage: e.message
       })
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location; // Last part is the query string name
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location; // Last part is the query string name
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
